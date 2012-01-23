548bf9e8f539f4ceac60bcf38842ca6aa4ac5531
Gregory Jacobs <greg@greg-jacobs.com>
2012-01-22 22:09:11 -0500

    Clean up documentation of Kevlar.util.DelayedTask.

--- js/util/DelayedTask.js
+++ js/util/DelayedTask.js
@@ -1,31 +1,35 @@
 /**
  * @class Kevlar.util.DelayedTask
  *
- * <p> The DelayedTask class provides a convenient way to "buffer" the execution of a method,
+ * The DelayedTask class provides a convenient way to "buffer" the execution of a method,
  * performing setTimeout where a new timeout cancels the old timeout. When called, the
- * task will wait the specified time period before executing. If durng that time period,
+ * task will wait the specified time period before executing. If during that time period,
  * the task is called again, the original call will be cancelled. This continues so that
- * the function is only called a single time for each iteration.</p>
- * <p>This method is especially useful for things like detecting whether a user has finished
+ * the function is only called a single time for each iteration.
+ * 
+ * This method is especially useful for things like detecting whether a user has finished
  * typing in a text field. An example would be performing validation on a keypress. You can
  * use this class to buffer the keypress events for a certain number of milliseconds, and
- * perform only if they stop for that amount of time.  Usage:</p><pre><code>
-var task = new Kevlar.util.DelayedTask(function(){
-	alert(Kevlar.getDom('myInputField').value.length);
-});
-// Wait 500ms before calling our function. If the user presses another key 
-// during that 500ms, it will be cancelled and we'll wait another 500ms.
-Kevlar.get('myInputField').on('keypress', function(){
-	task.{@link #delay}(500); 
-});
- * </code></pre> 
- * <p>Note that we are using a DelayedTask here to illustrate a point. The configuration
+ * perform only if they stop for that amount of time.  Usage:
+ * 
+ *     var task = new Kevlar.util.DelayedTask( function() {
+ *         alert( document.getElementById( 'myInputField' ).value.length );
+ *     } );
+ *     
+ *     // Wait 500ms before calling our function. If the user presses another key 
+ *     // during that 500ms, it will be cancelled and we'll wait another 500ms.
+ *     document.getElementById( 'myInputField' ).onkeypress = function() {
+ *         task.delay( 500 ); 
+ *     } );
+ *     
+ * Note that we are using a DelayedTask here to illustrate a point. The configuration
  * option `buffer` for {@link Kevlar.util.Observable#addListener addListener/on} will
- * also setup a delayed task for you to buffer events.</p> 
+ * also setup a delayed task for you to buffer events.
+ *  
  * @constructor The parameters to this constructor serve as defaults and are not required.
  * @param {Function} fn (optional) The default function to call.
- * @param {Object} scope (optional) The default scope (The <code><b>this</b></code> reference) in which the
- * function is called. If not specified, <code>this</code> will refer to the browser window.
+ * @param {Object} scope (optional) The default scope (The `this` reference) in which the
+ *   function is called. If not specified, `this` will refer to the browser window.
  * @param {Array} args (optional) The default Array of arguments.
  */
 /*global Kevlar */
