90e36d0dc0da81b50d0fd50e6a87ead667d7b7ff
Gregory Jacobs <greg@jux.com>
2012-02-10 18:15:27 -0500

    Add configs to Attribute for embedded models.

--- src/Attribute.js
+++ src/Attribute.js
@@ -130,6 +130,33 @@ Kevlar.Attribute = Kevlar.extend( Object, {
 	 */
 	persist : true,
 	
+	/**
+	 * @cfg {Boolean} embedded
+	 * If a {@link Kevlar.Model Model} is set to the attribute, setting this config to true has the parent {@link Kevlar.Model Model} treat the child 
+	 * {@link Kevlar.Model Model} as if it is a part of itself. Normally, a child Model that is not embedded is treated as a "relation", where it is
+	 * considered as independent from the parent Model.
+	 * 
+	 * So what this means is that, when true:
+	 * 
+	 * - The parent Model is considered as "changed" when an attribute in the child Model is changed. The attribute that holds the child
+	 *   model is the "change".
+	 * - The parent Model's {@link Kevlar.Model#change change} event is fired when an attribute on the child Model is changed.
+	 * - The child Model's data is persisted with the parent Model's data, unless the {@link #persistIdOnly} config is set to true,
+	 *   in which case just the child Model's {@link Kevlar.Model#idAttribute id} is persisted with the parent Model.
+	 * 
+	 * Note that this configuration may be a part of a special "ModelAttribute" subclass in the future.
+	 */
+	embedded : false,
+	
+	/**
+	 * @cfg {Boolean} persistIdOnly
+	 * In the case that an {@link #embedded} {@link Kevlar.Model Model} is set to the attribute, set this to true to only have the 
+	 * {@link Kevlar.Model#idProperty id} of the embedded model be persisted, rather than all of the Model data.
+	 */
+	persistIdOnly : false,
+	
+	
+	
 	
 	/**
 	 * Creates a new Attribute instance. Note: You will normally not be using this constructor function, as this class
