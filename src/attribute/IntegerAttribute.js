74c3aa912af4df3e9122d4934f938844f7991726
Gregory Jacobs <greg@jux.com>
2012-02-14 13:27:25 -0500

    Rename IntAttribute to IntegerAttribute.

---
+++ src/attribute/IntegerAttribute.js
@@ -0,0 +1,16 @@
+/**
+ * @class Kevlar.attribute.IntegerAttribute
+ * @extends Kevlar.attribute.Attribute
+ * 
+ * Attribute definition class for an Attribute that takes an integer data value.
+ */
+/*global Kevlar */
+Kevlar.attribute.IntegerAttribute = Kevlar.attribute.Attribute.extend( {
+		
+	
+	
+} );
+
+
+// Register the Attribute type
+Kevlar.attribute.Attribute.registerType( 'int', Kevlar.attribute.IntegerAttribute );
